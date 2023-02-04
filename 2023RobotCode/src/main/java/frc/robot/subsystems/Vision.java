6d72e156e397c1d7ca94d380ed3a8b2efdae2940
Ben H <bhargrove09@gmail.com>
2023-02-04 12:14:57 -0500

    Finds running average of pose

--- 2023RobotCode/src/main/java/frc/robot/subsystems/Vision.java
+++ 2023RobotCode/src/main/java/frc/robot/subsystems/Vision.java
@@ -5,7 +5,9 @@
 package frc.robot.subsystems;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonUtils;
@@ -27,10 +29,17 @@ public class Vision extends SubsystemBase {
   private PhotonCamera piCam = new PhotonCamera("piCam");
 
   //Variables to keep track of stuff
+  private final int runningAverageSize = 15;
+  private Queue<Double> RAPoseX = new LinkedList<Double>();
+  private Queue<Double> RAPoseY = new LinkedList<Double>();
+  private Queue<Double> RAPoseSin = new LinkedList<Double>();
+  private Queue<Double> RAPoseCos = new LinkedList<Double>();
+  private int numberOfTargets = 0;
+  private boolean hasValue = false;
   private double poseX = 0;
   private double poseY = 0;
-  private Rotation2d rotation = null;
-  private int numberOfTargets = 0;
+  private Rotation2d poseRotation = new Rotation2d();
+
 
   /** Creates a new Vision. */
   public Vision() {
@@ -46,8 +55,9 @@ public class Vision extends SubsystemBase {
     PhotonPipelineResult piCamInfo = piCam.getLatestResult(); //Get most recent info from camera
     boolean piCamHasTargets = piCamInfo.hasTargets(); //Checks if it actually sees any targets
     if (piCamHasTargets) {
-      ArrayList<Pose2d> allPose2ds = new ArrayList<Pose2d>();
+      
       List<PhotonTrackedTarget> targets = piCamInfo.getTargets();
+
       for (PhotonTrackedTarget t : targets) {
         //Get info from tag
         int id_number = t.getFiducialId();  //Apriltag ID
@@ -57,37 +67,47 @@ public class Vision extends SubsystemBase {
         Pose3d tagPose = AprilTagPoses.getPose(id_number);  //Get tag's position on field
         if(tagPose == null){
           continue; //Bad tag, so go to next loop
-        }
-        Transform3d robotToCameraPose = CameraPoses.getCameraPose(); //Get Camera's position relative to tag
-        Pose3d robotPose = PhotonUtils.estimateFieldToRobotAprilTag(cameraToTargetPose, tagPose, robotToCameraPose); //Get robot's position on field
-        allPose2ds.add(robotPose.toPose2d());
-      }
+        } else{
+          //Get robot's position
+          Transform3d robotToCameraPose = CameraPoses.getCameraPose(); //Get Camera's position relative to tag
+          Pose3d robotPose3d = PhotonUtils.estimateFieldToRobotAprilTag(cameraToTargetPose, tagPose, robotToCameraPose); //Get robot's position on field
+          Pose2d robotPose2d = robotPose3d.toPose2d();
 
-      numberOfTargets = allPose2ds.size();
-      if (numberOfTargets == 1){
-        Pose2d tagPose = allPose2ds.get(0);
-        poseX = tagPose.getX();
-        poseY = tagPose.getY();
-        rotation = tagPose.getRotation();
-      } else if (numberOfTargets > 1){
-        //Get average rotation and x,y
-        double totalX = 0;
-        double totalY = 0;
-        double totalSin = 0;
-        double totalCos = 0;
-        for(Pose2d pose : allPose2ds){
-          totalX += pose.getX();
-          totalY += pose.getY();
-          totalSin += pose.getRotation().getSin();
-          totalCos += pose.getRotation().getCos();
+          //Update running average
+          RAPoseX.add(robotPose2d.getX());
+          RAPoseY.add(robotPose2d.getY());
+          RAPoseCos.add(robotPose2d.getRotation().getCos());
+          RAPoseSin.add(robotPose2d.getRotation().getSin());
+          numberOfTargets ++;
         }
-
-        poseX = totalX / numberOfTargets;
-        poseY = totalY / numberOfTargets;
-        double averageSin = totalSin / numberOfTargets;
-        double averageCos = totalCos / numberOfTargets;
-        rotation = new Rotation2d(averageCos, averageSin);
       }
+    }
+
+    //Add null frame when robot doesn't see anything
+    if (numberOfTargets == 0){
+      RAPoseX.add(Double.NaN);
+      RAPoseY.add(Double.NaN);
+      RAPoseSin.add(Double.NaN);
+      RAPoseCos.add(Double.NaN);
+    }
+
+    //Delete old data to keep running average
+    while(RAPoseX.size() > runningAverageSize){
+      RAPoseX.remove();
+      RAPoseY.remove();
+      RAPoseSin.remove();
+      RAPoseCos.remove();
+    }
+
+    //Get average position of robot
+    Double poseX_median = getMedian(RAPoseX);
+    if(poseX_median == Double.NaN){
+      hasValue = false;
+    } else{
+      hasValue = true;
+      poseX = poseX_median;
+      poseY = getMedian(RAPoseY);
+      poseRotation = new Rotation2d(getMedian(RAPoseCos), getMedian(RAPoseSin));
     }
 
     //Update network tables
@@ -96,10 +116,10 @@ public class Vision extends SubsystemBase {
 
     //Write values to network table
     nt.getTable("Vision").getEntry("Number of Tags").setValue(numberOfTargets);
-    if(numberOfTargets > 0){
+    if(hasValue){
       nt.getTable("Vision").getEntry("X").setValue(poseX);
       nt.getTable("Vision").getEntry("Y").setValue(poseY);
-      nt.getTable("Vision").getEntry("Rotation").setValue(rotation.toString());
+      nt.getTable("Vision").getEntry("Rotation").setValue(poseRotation.toString());
     } else {
       nt.getTable("Vision").getEntry("X").unpublish();
       nt.getTable("Vision").getEntry("Y").unpublish();
@@ -114,7 +134,7 @@ public class Vision extends SubsystemBase {
    */
   public boolean updateOdomentry(DriveTrain driveTrain){
     if (numberOfTargets >= 1) {
-      Pose2d pose = new Pose2d(poseX, poseY, rotation);
+      Pose2d pose = new Pose2d(poseX, poseY, poseRotation);
       driveTrain.resetOdometry(pose);
 
       NetworkTableInstance nt = NetworkTableInstance.getDefault();
@@ -126,6 +146,51 @@ public class Vision extends SubsystemBase {
       nt.getTable("Vision").getEntry("reset").setValue(false);
 
       return false;
+    }
+  }
+
+  /**
+   * Find the median of a running average data set
+   * @param runningAverage a Queue<Double> for the running average
+   * @return the median (Double) or Double.NaN if there are no good values
+   */
+  private Double getMedian(Queue<Double> runningAverage){
+    ArrayList<Double> ordered = new ArrayList<Double>();
+    int goodTags = 0;
+
+    //Sort all the good values into ordered
+    for (Double value : runningAverage){
+      if (value == Double.NaN){
+        continue; //Empty frame
+      } else{
+        //Found another good tag
+        goodTags ++;
+
+        //Sort value into ordered
+        for(int i = 0; i < ordered.size(); i++){
+          if(value < ordered.get(i)) {
+            ordered.add(i, value);
+            break;
+          }
+        }
+
+        //Make sure the value was added
+        if (ordered.size() != goodTags){
+          ordered.add(value);
+        }
+      }
+    }
+
+    //Find median
+    if(goodTags == 0){
+      return Double.NaN;  //There are no values to find the median of
+    } else if(goodTags % 2 == 1){ //There is an odd number of values
+      int index = (goodTags - 1)/2;
+      return ordered.get(index);
+    } else{ //There is an even number of values
+      int index1 = goodTags/2;
+      int index2 = index1-1;
+      return (ordered.get(index1) + ordered.get(index2))/2.0;
     }
   }
 }
